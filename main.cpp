44e3d7360e669a8152b754dea2a5a4da1489ec1b
Xechteg <114721085+Xechteg@users.noreply.github.com>
2024-08-26 00:00:08 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  setlocale(LC_ALL, "rus");
+  int n,d;
+  cout << "Введите 1 число: ";
+  cin >> n;
+  cout << "Введите 2 число: ";
+  cin >> d;
+  cout << "Сумма этих чисел >" << n + d;
+}
